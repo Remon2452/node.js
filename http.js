d007e32336c54d59888e370db627f386ba8d52f3
Shahadat Hossain Remon <shremonmirza@gmail.com>
2026-08-09 01:12:37 +0600

    server

--- http.js
+++ http.js
@@ -1,4 +1,4 @@
-const http = require('http');
+/*const http = require('http');
 
 const server = http.createServer();
 
@@ -8,4 +8,49 @@ server.on('connection', (socket) => {
 
 server.listen(3000, () => {
     console.log('Listening on port 3000...');
+});*/
+
+/*const http = require('http');
+
+const server = http.createServer();
+console.log('serc=ver created');*/
+
+/*const http=require('http');
+const server = http.createServer();
+server.listen(3000);
+console.log('server is running');*/
+
+
+//full server
+
+/*const http = require('http');
+const server = http.createServer((req,res)=>{
+    res.write('Shahadat hossain Remon');
+    res.end();
+});
+server.listen(3000,()=>{
+    console.log('server is running on port 3000');
+});*/
+
+
+
+
+//practice
+
+const http = require('http');
+
+const server = http.createServer((req,res)=>{
+    if(req.url==='/'){
+        res.end('home page');
+    }
+    else if(req.url==='/about'){
+        res.end('about page');
+    }
+    else{
+        res.end('404 - page not found');
+    }
+});
+
+server.listen(3000, ()=>{
+    console.log('server running....');
 });
--- module.js
+++ module.js
@@ -1,2 +1,21 @@
-console.log(__filename);
-console.log(__dirname);
+/*console.log(__filename);
+console.log(__dirname);*/
+
+
+
+/*const path = require('path');
+
+const filepath = path.join(__dirname,'student.txt');
+console.log(filepath);*/
+
+
+/*const path = require('path');
+
+const filePath = '/home/remon/project/index.js';
+
+console.log(path.basename(filePath));*/
+
+const fs = require('fs');
+const path = require('path');
+const filepath = path.join(__dirname,'student.txt');
+
